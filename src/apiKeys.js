960d867aa3d3e400e36bcff20cd7dfcab0091a56
ThunderSmoker <pitlawararadhya@gmail.com>
2023-10-13 12:12:16 +0530

    jk

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "0c8166fb18ced5ff35c6e776dfca39ee",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
